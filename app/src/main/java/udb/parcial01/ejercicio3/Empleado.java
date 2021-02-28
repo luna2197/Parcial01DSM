d705f699fc933bbb3bd32fe527d62d9acd0a4cc6
luna2197 <robertoluna_37@hotmail.com>
2021-02-28 00:03:09 -0600

    correccion de errores

--- app/src/main/java/udb/parcial01/ejercicio3/Empleado.java
+++ app/src/main/java/udb/parcial01/ejercicio3/Empleado.java
@@ -5,17 +5,57 @@ public class Empleado {
   private  String apellido;
   private  String cargo;
   private int horas;
+  private  double sueldol;
+  private double isss;
+    private double afp;
+    private double renta;
 
     public  Empleado(){
 
     }
 
-    public  Empleado(String nombre, String apellido, String cargo, int horas){
+    public  Empleado(String nombre, String apellido, String cargo, int horas, double sueldol, double isss, double afp , double renta){
         this.nombre = nombre;
         this.apellido = apellido;
         this.cargo = cargo;
         this.horas = horas;
+        this.sueldol = sueldol;
+        this.isss = isss;
+        this.afp = afp;
+        this.renta = renta;
 
+    }
+
+    public double getSueldol() {
+        return sueldol;
+    }
+
+    public double getIsss() {
+        return isss;
+    }
+
+    public double getAfp() {
+        return afp;
+    }
+
+    public double getRenta() {
+        return renta;
+    }
+
+    public void setSueldol(double sueldol) {
+        this.sueldol = sueldol;
+    }
+
+    public void setIsss(double isss) {
+        this.isss = isss;
+    }
+
+    public void setAfp(double afp) {
+        this.afp = afp;
+    }
+
+    public void setRenta(double renta) {
+        this.renta = renta;
     }
 
     public String getNombre() {
@@ -48,6 +88,10 @@ public class Empleado {
 
     public void setHoras(int horas) {
         this.horas = horas;
+    }
+    @Override
+    public String toString() {
+        return nombre + "  ISSS: $"+ isss+ "  AFP: $"+afp+"  Renta: $" +renta+"  Sueldo Liquido: $"+sueldol;
     }
 }
 
--- app/src/main/java/udb/parcial01/ejercicio3/MainActivity.java
+++ app/src/main/java/udb/parcial01/ejercicio3/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText nombre, apellido, cargo, horas;
     private ListView datos;
-    private Button ingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +39,49 @@ public class MainActivity extends AppCompatActivity {
         String ape = apellido.getText().toString();
         String car = cargo.getText().toString();
         int hrs = Integer.parseInt(horas.getText().toString());
+        double sueldob = 0, isss= 0, afp=0, renta =0, sueldol=0;
+
+        if(hrs <= 0){  Toast msg = Toast.makeText(this, "Las hora no pueden ser menores a 0", Toast.LENGTH_LONG);
+            msg.show(); }
+        else{
+
+            if (hrs <= 160){ sueldob =hrs*9.75;}
+            if (hrs > 160){ sueldob =1560 +(hrs-160) * 11.75;}
+
+            isss = (sueldob * 5.25)/100;
+            afp = (sueldob * 6.8)/100;
+            renta = (sueldob * 10)/100;
+
+            sueldol = sueldob - isss -afp - renta;
+
+            if(car == "Gerente"){ sueldol = sueldol + ((sueldol*0.1));}
+            if(car == "Asistente"){ sueldol = sueldol + ((sueldol*0.05));}
+            if(car == "Secretaria"){ sueldol = sueldol + ((sueldol*0.03));}
+            else{ sueldol = sueldol + ((sueldol*0.02)); }
 
 
-        lista.add(new Empleado(nom, ape, car, hrs));
+
+        lista.add(new Empleado(nom, ape, car, hrs, sueldol, isss, afp, renta));
         contador++;
-        if(contador ==3){
+
+            Toast msg = Toast.makeText(this, "Datos Ingresados", Toast.LENGTH_LONG);
+            msg.show();
+
+        if(contador == 3){
             Button  ingresar = (Button) findViewById(R.id.btn_ingresar);
             ingresar.setEnabled(false);
         }
+
+        nombre.setText("");
+        apellido.setText("");
+        cargo.setText("");
+        horas.setText("");
+        }
+
+    }
+
+    public void Mostar(View view){
+        ArrayAdapter <Empleado> adaptador = new ArrayAdapter<Empleado>(MainActivity.this, android.R.layout.simple_list_item_1, lista);
+        datos.setAdapter(adaptador);
     }
 }
